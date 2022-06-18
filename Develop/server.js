03bbf212ac1ada38f262da5c1e3a27a3dda375a9
AndrewCryer <son_of_loki@hotmail.com>
2022-06-17 22:32:20 -0400

    Further work trying to build the GET function and required APIs on my Server.js sheet. Also fixed CSS file path in Routes.html

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { get } = require('https');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);

19657e6d72acd474137b5dbd14eb32e0d2ab5d23
AndrewCryer <son_of_loki@hotmail.com>
2022-06-19 23:29:40 -0400

    Still trying to fix the routing.  Having a cors error but found a working version to figure out what ive done wrong

--- Develop/server2.js
+++ Develop/server2.js
@@ -4,14 +4,14 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 8080;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`);
+    console.log(`Now listening on PORT: ${PORT}`);
 });

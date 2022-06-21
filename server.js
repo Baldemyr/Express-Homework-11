582ea314bd0354a059330982c6ee61eafa7b1aa6
AndrewCryer <son_of_loki@hotmail.com>
2022-06-20 21:24:54 -0400

    Trying a new port since Heroku error log claimed this port didnt respond

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

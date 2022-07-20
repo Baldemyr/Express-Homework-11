876f48c62a674504db03df1589b0cebed7059634
Baldemyr <son_of_loki@hotmail.com>
2022-07-19 20:34:05 -0400

    Fixed weird Port issue

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+//declare variables + required libraries 
 
-const app = express();
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 8080;
+// Connect to route folders
 
+let apiRoutes = require("./routes/apiRoutes");
+let htmlRoutes = require("./routes/htmlRoutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Now listening on PORT: ${PORT}`);
+//set up to use insomnia to test 
+
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
 });
